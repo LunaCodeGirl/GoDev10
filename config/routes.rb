5799e61b619db3e23a038d56b55a1fd7ab2f4416
Spencer Comerford <Spencevail@gmail.com>
2014-05-18 06:34:20 -0600

    Installed rails_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 GoDev10::Application.routes.draw do
   
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
